a61177cd5d739b35a5c63e7471bd5d3b73ea7c86
longbai <slongbai@gmail.com>
2014-06-15 22:18:10 +0800

    add direct upload

--- src/qiniuput/main.go
+++ src/qiniuput/main.go
@@ -8,13 +8,44 @@ import (
 )
 
 import (
-	"github.com/qiniu/api/resumable/io"
+	"github.com/qiniu/api/io"
+	rio "github.com/qiniu/api/resumable/io"
 )
+
+func resumblePut(token, key, file string, params map[string]string) (err error) {
+	blockNotify := func(blkIdx int, blkSize int, ret *rio.BlkputRet) {
+		log.Println("block id", blkIdx, "offset", ret.Offset)
+	}
+	extra := &rio.PutExtra{
+		ChunkSize: 8192,
+		Notify:    blockNotify,
+	}
+	if len(params) != 0 {
+		extra.Params = params
+	}
+
+	var ret rio.PutRet
+	return rio.PutFile(nil, &ret, token, key, file, extra)
+
+}
+
+func put(token, key, file string, params map[string]string) (err error) {
+	var extra *io.PutExtra
+	if len(params) != 0 {
+		extra = &io.PutExtra{
+			Params: params,
+		}
+	}
+
+	var ret io.PutRet
+	return io.PutFile(nil, &ret, token, key, file, extra)
+}
 
 func main() {
 	token := flag.String("t", "", "upload token")
 	file := flag.String("f", "", "file path")
 	key := flag.String("k", "", "file key")
+	resume := flag.Bool("c", false, "断点续传")
 	custom := flag.String("x", "", "custom args foo=1&bar=2")
 	flag.Parse()
 	if *token == "" || *file == "" || *key == "" {
@@ -33,17 +64,10 @@ func main() {
 		log.Fatalln("invalid file")
 		return
 	}
-
-	blockNotify := func(blkIdx int, blkSize int, ret *io.BlkputRet) {
-		log.Println("size", stat.Size(), "block id", blkIdx, "offset", ret.Offset)
-	}
+	log.Println("file size", stat.Size())
 
 	params := map[string]string{}
-	extra := &io.PutExtra{
-		ChunkSize: 8192,
-		Notify:    blockNotify,
-		Params:    params,
-	}
+
 	if custom != nil && *custom != "" {
 		values, err := url.ParseQuery(*custom)
 		if err != nil {
@@ -53,10 +77,13 @@ func main() {
 		for k, v := range values {
 			params["x:"+k] = v[0]
 		}
-		extra.Params = params
 	}
-	var ret io.PutRet
-	err = io.PutFile(nil, &ret, *token, *key, *file, extra)
+
+	if *resume {
+		err = resumblePut(*token, *key, *file, params)
+	} else {
+		err = put(*token, *key, *file, params)
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
